61133987dce13bc7474d0396fb0534399b22c3e5
alihoda <a.hodavand@yahoo.com>
2021-05-24 21:05:38 +0430

    add product-list and product-detail pathes and their views

---
+++ web/backend/products/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import fields, serializers
+
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.name')
+
+    class Meta:
+        model = Product
+        fields = '__all__'
--- web/backend/products/views.py
+++ web/backend/products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
+
+
+class ProductView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
