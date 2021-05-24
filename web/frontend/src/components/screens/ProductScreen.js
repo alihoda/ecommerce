45f736da9aaba7611f15dc3ea2330a716279ad4c
alihoda <a.hodavand@yahoo.com>
2021-05-24 21:44:35 +0430

    change product detail card position

--- web/frontend/src/components/screens/ProductScreen.js
+++ web/frontend/src/components/screens/ProductScreen.js
@@ -29,70 +29,71 @@ function ProductScreen({ match }) {
         </Col>
         {/* Cart and product detail col */}
         <Col md={5}>
-          <Col>
-            <Card>
-              <ListGroup variant="flush">
-                <ListGroup.Item className="d-flex justify-content-between">
-                  <span>Price:</span>
-                  <span>${product.price}</span>
-                </ListGroup.Item>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item className="d-flex justify-content-between">
+                <span>Price:</span>
+                <span>${product.price}</span>
+              </ListGroup.Item>
 
-                <ListGroup.Item className="d-flex justify-content-between">
-                  <span>Status:</span>
-                  <span>
-                    {product.countInStock > 0 ? "In stock" : "Out of stock"}
-                  </span>
-                </ListGroup.Item>
+              <ListGroup.Item className="d-flex justify-content-between">
+                <span>Status:</span>
+                <span>
+                  {product.countInStock > 0 ? "In stock" : "Out of stock"}
+                </span>
+              </ListGroup.Item>
 
-                <ListGroup.Item className="d-flex flex-column">
-                  <Button
-                    className="btn-light"
-                    type="button"
-                    disabled={product.countInStock == 0}
-                  >
-                    Add to Cart
-                  </Button>
-                </ListGroup.Item>
-              </ListGroup>
-            </Card>
-            <hr />
-          </Col>
-
-          <Col className="py-1">
-            <Card>
-              <ListGroup variant="flush">
-                <ListGroup.Item>
-                  <Rating
-                    value={product.rating}
-                    text={`${product.numReviews} reviews`}
-                    color={"#f8e825"}
-                  />
-                </ListGroup.Item>
-
-                <ListGroup.Item className="d-flex justify-content-between">
-                  <span>Brand:</span>
-                  <span>{product.brand}</span>
-                </ListGroup.Item>
-
-                <ListGroup.Item className="d-flex justify-content-between">
-                  <span>Price:</span>
-                  <span>${product.price}</span>
-                </ListGroup.Item>
-              </ListGroup>
-            </Card>
-          </Col>
+              <ListGroup.Item className="d-flex flex-column">
+                <Button
+                  className="btn-light"
+                  type="button"
+                  disabled={product.countInStock == 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
 
-      <Card className="my-3">
-        <ListGroup variant="flush">
-          <ListGroup.Item>
-            <h2>{product.name}</h2>
-          </ListGroup.Item>
+      <Row className="my-3">
+        <Col md={7}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>{product.name}</h2>
+              </ListGroup.Item>
 
-          <ListGroup.Item>{product.description}</ListGroup.Item>
-        </ListGroup>
-      </Card>
+              <ListGroup.Item>{product.description}</ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+
+        <Col md={5}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                  color={"#f8e825"}
+                />
+              </ListGroup.Item>
+
+              <ListGroup.Item className="d-flex justify-content-between">
+                <span>Brand:</span>
+                <span>{product.brand}</span>
+              </ListGroup.Item>
+
+              <ListGroup.Item className="d-flex justify-content-between">
+                <span>Price:</span>
+                <span>${product.price}</span>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
