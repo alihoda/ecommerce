a23ac239b0b509cfee4a58783ca4cc63b7615fda
alihoda <a.hodavand@yahoo.com>
2021-05-24 13:27:12 +0430

    add Product model and register in admin

--- web/backend/products/models.py
+++ web/backend/products/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self) -> str:
+        return self.name
