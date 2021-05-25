e72100d428b605cfea04b85f7feaaf54153dd3a6
alihoda <a.hodavand@yahoo.com>
2021-05-25 13:38:59 +0430

    add spinner loader and message components

--- web/frontend/src/components/screens/HomeScreen.js
+++ web/frontend/src/components/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 
 import Product from "../Product";
+import Loader from "../Loader";
+import Message from "../Message";
 import { listProducts } from "../../actions/productActions";
 
 function HomeScreen() {
@@ -18,9 +20,9 @@ function HomeScreen() {
         <div>
             <h1>Latest Products</h1>
             {loading ? (
-                <h2>Loading ...</h2>
+                <Loader />
             ) : error ? (
-                <h3>{error}</h3>
+                <Message variant="danger">{error}</Message>
             ) : (
                 <Row>
                     {products.map((product) => (
